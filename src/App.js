e800e7bdf891465d1183b634ce80791c1853b891
Daris <daris.hodzic.dh@gmail.com>
2021-09-22 14:35:11 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 //Importing components
-
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+	const [inputText, setInputText] =
+		useState("");
+	const [todos, setTodos] = useState(
+		[]
+	);
 	return (
 		<div className="App">
 			<header>
 				<h1>React Todo List</h1>
 			</header>
-			<Form />
+			<Form
+				todos={todos}
+				setTodos={setTodos}
+				inputText={inputText}
+				setInputText={setInputText}
+			/>
 			<TodoList />
 		</div>
 	);
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,39 @@
 import React from "react";
 
-const Form = () => {
+const Form = ({
+	inputText,
+	setInputText,
+	todos,
+	setTodos,
+}) => {
+	const inputTextHandler = (e) => {
+		console.log(e.target.value);
+		setInputText(e.target.value);
+	};
+
+	const submitTodoHandler = (e) => {
+		e.preventDefault();
+		setTodos([
+			...todos,
+			{
+				text: inputText,
+				completed: false,
+				id: Math.random() * 1000,
+			},
+		]);
+		setInputText("");
+	};
+
 	return (
 		<form>
 			<input
+				value={inputText}
 				type="text"
 				className="todo-input"
+				onChange={inputTextHandler}
 			/>
 			<button
+				onClick={submitTodoHandler}
 				className="todo-button"
 				type="submit"
 			>
